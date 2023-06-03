e3d757b1d5277d6d3038e74c9d1eb7cd859a532d
Павел Смирнов <cocacola76rus@gmail.com>
2023-06-03 23:14:30 +0300

    Закончил с конфертацией из числа в римское число.

--- RomanNumeralsHelper/NumeralToRomanNumeralConverter.cs
+++ RomanNumeralsHelper/NumeralToRomanNumeralConverter.cs
@@ -2,39 +2,26 @@
 {
     public class NumeralToRomanNumeralConverter: IConverter
     {
-        private const int COST_M = 1000;
-        private const string LITER_M = "M";
-        private const int COST_D = 500;
-        private const string LITER_D = "D";
-        private const int COST_C = 100;
-        private const string LITER_C = "C";
-        private const int COST_L = 50;
-        private const string LITER_L = "L";
-        private const int COST_X = 10;
-        private const string LITER_X = "X";
-        private const int COST_IX = 9;
-        private const string LITER_IX = "IX";
+        private readonly Dictionary<string, int> romanNumeralAlphabet = new Dictionary<string, int>
+        {
+            { "M", 1000 }, { "D", 500 }, { "C", 100 }, { "L", 50 }, { "X", 10 }, { "IX", 9 }, { "VIII", 8 }, { "VII", 7 }, { "VI", 6 }, { "V", 5 },
+            { "IV", 4 },{ "III", 3 },{ "II", 2 },{ "I", 1 }
+        };
+
         public string Convert(int n)
         {
             var romanNumeralString = "";
 
-            romanNumeralString += PickOutOneRomanNumber(n, COST_M, LITER_M);
-            n = n % COST_M;
-            romanNumeralString += PickOutOneRomanNumber(n, COST_D, LITER_D);
-            n = n % COST_D;
-            romanNumeralString += PickOutOneRomanNumber(n, COST_C, LITER_C);
-            n = n % COST_C;
-            romanNumeralString += PickOutOneRomanNumber(n, COST_L, LITER_L);
-            n = n % COST_L;
-            romanNumeralString += PickOutOneRomanNumber(n, COST_X, LITER_X);
-            n = n % COST_X;
-            romanNumeralString += PickOutOneRomanNumber(n, COST_IX, LITER_IX);
-            n = n % COST_IX;
+            foreach(var liter in romanNumeralAlphabet)
+            {
+                romanNumeralString += PickOutOneRomanNumber(n, liter.Value, liter.Key);
+                n %= liter.Value;
+            }
 
             return romanNumeralString;
         }
 
-        private string PickOutOneRomanNumber(int n, int costRomanNumber, string literalRomanNumber)
+        private static string PickOutOneRomanNumber(int n, int costRomanNumber, string literalRomanNumber)
         {
             var romanNumberCount = n / costRomanNumber;
 
--- RomanNumeralsHelperTests/NumeralToRomanNumeralConverterTests.cs
+++ RomanNumeralsHelperTests/NumeralToRomanNumeralConverterTests.cs
@@ -78,5 +78,104 @@ namespace RomanNumeralsHelperTests
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void Convert_8_VIII()
+        {
+            var number = 8;
+            var expected = "VIII";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_7_VII()
+        {
+            var number = 7;
+            var expected = "VII";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_6_VI()
+        {
+            var number = 6;
+            var expected = "VI";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_5_V()
+        {
+            var number = 5;
+            var expected = "V";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_4_IV()
+        {
+            var number = 4;
+            var expected = "IV";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_3_III()
+        {
+            var number = 3;
+            var expected = "III";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_2_II()
+        {
+            var number = 2;
+            var expected = "II";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_1_I()
+        {
+            var number = 1;
+            var expected = "I";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Convert_1666_MDCLXVI()
+        {
+            var number = 1666;
+            var expected = "MDCLXVI";
+
+            var actual = new NumeralToRomanNumeralConverter().Convert(number);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
